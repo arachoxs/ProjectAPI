bdeff634d069999a09daad96d1a95fd0499dda45
arachoxs <140642177+arachoxs@users.noreply.github.com>
2024-09-19 22:49:43 -0500

    eliminacion data cleaning

--- UI/__interface__.py
+++ UI/__interface__.py
@@ -16,35 +16,11 @@ def info_user():
 
     return info
 
+
 def show_data(data_api, amount_to_show):
     # me permite mostrar toda la cantidad de datos
     print("\n\n")
     pd.set_option('display.max_rows', int(amount_to_show))
     print(data_api)
     
-def frame_inf(data_api):
-    #informacion general de lo que conforma el data frame
-    print("numero de filas->", data_api.shape[0])
-    print("\n")
-    print("numero de columnas->", data_api.shape[1])
-    print("\n")
-    print("Nombre de las columnas->", data_api.columns.values.tolist())
-    print("\n")
-    print("Tipo de datos de columna->\n",data_api.dtypes)
     
-    #informacion sobre los valores faltantes en el data frame
-    columnas_NA=data_api.columns[data_api.isnull().any()].tolist()
-    print("\n\n")
-    print("Columns con valores faltantes-> ", columnas_NA)
-    filas_con_na = data_api[data_api.isnull().any(axis=1)].index.tolist() 
-    print("\n\n")
-    print("Numero de filas con valores faltantes -> ", len(filas_con_na))
-    print("\n\n")
-    print("Indices de ejemplo con datos faltantes ->", filas_con_na[:5]) 
-    print("\n\n")
-    print("Informacion resumida:")
-    print(data_api.info())
-    print("\n\n")
-    print("Estadísticas resumidas:" )
-    print(data_api.describe())
-        
